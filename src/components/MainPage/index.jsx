a77299c32cde40d01e451099e16087c8681d7247
EC2 Default User <ec2-user@ip-172-31-95-121.ec2.internal>
2025-01-31 03:01:33 +0000

    alternado para Produtos disponiveis

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -90,7 +90,7 @@ const CloudMartMainPage = () => {
       <Header />
       <main className="container mx-auto py-8 flex-grow px-4">
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-bold">Featured Products</h2>
+          <h2 className="text-2xl font-bold">Featured Products (Produtos Disponiveis)</h2>
           <div className="relative">
             <input
               type="text"
@@ -100,7 +100,7 @@ const CloudMartMainPage = () => {
               onChange={handleSearch}
             />
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
-          </div>
+i          </div>
         </div>
 
         {loading ? (
